339535dcd3c8abebc27695373cdd4b1a5570ece5
Logesh-Devops <55657365+Logesh-Devops@users.noreply.github.com>
2025-08-29 02:26:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import '@fontsource/poppins/400.css';
@@ -10,6 +10,8 @@ import '@fontsource/poppins/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
